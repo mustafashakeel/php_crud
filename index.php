bb1e6555755340716306e644c8346062a85ab91b
Mustafa Qureshi <mustafashakeel@gmail.com>
2022-01-13 16:03:31 -0800

    initial code

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP CRUD Application </title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h2>PHP CRUD Appplication </h2>
+        </header>
+
+        <main>
+
+
+        </main>
+    </div>
+</body>
+
+</html>
