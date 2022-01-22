bf7da1fad12bff3e8b49d55542bdfee4ce8ba540
Mustafa Qureshi <mustafashakeel@gmail.com>
2022-01-21 16:05:39 -0800

    view and add udpated

--- function.php
+++ function.php
@@ -3,28 +3,73 @@
 class Student
 {
 
-    private  $host = 'localhost';
-    private $username = 'root';
+    private $host = 'localhost';
+    private $username  = 'root';
     private $password = 'root';
-    private $dbname = 'students';
+    private $dbName = 'students';
     private $port = 8889;
 
     public $conn;
-    function __construct()
+    // Database Connection
+
+    public function __construct()
     {
-        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->dbname);
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->dbName);
 
         if (mysqli_connect_error()) {
-            trigger_error(" Error is in DB " . mysqli_connect_error());
-        } {
+            trigger_error('Error in DB' . mysqli_connect_error());
+        } else {
             return $this->conn;
         }
     }
     public function viewData()
     {
+        $sql = "SELECT * FROM students";
+        $result = $this->conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $data = array();
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+
+            return $data;
+        }
     }
     public function add()
     {
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $email = $_POST['email'];
+        $phone = (int) $_POST['phone'];
+        $address = $_POST['address'];
+        $course = $_POST['course'];
+
+        $stmt = $this->conn->prepare("INSERT INTO students (first_name, last_name, phone, email, address, course) VALUES (?, ?, ?,?,?,?)");
+        $stmt->bind_param("ssisss", $first_name, $last_name, $phone, $email,  $address,  $course);
+
+        $stmt->execute();
+
+        if ($stmt = true) {
+            echo " Successs";
+        }
+
+
+        // $sql = "INSERT INTO students ( first_name, last_name, phone, email, address, course ) VALUES ( $first_name, $last_name , $phone, $email,  $address,  $course)";
+
+
+        // $sql = "INSERT INTO students (first_name, last_name, phone, email, address, course)
+        // VALUES ($first_name, $last_name , $phone, $email,  $address,  $course  )";
+
+
+
+
+
+        // if ($this->conn->query($sql) === TRUE) {
+        //     echo "New record created successfully";
+        // } else {
+        //     echo "Error: " . $sql . "<br>" . $this->conn->error;
+        // }
     }
     public function delete()
     {
--- index.php
+++ index.php
@@ -2,6 +2,38 @@
 
 include("function.php");
 
+$stdObj = new Student();
+
+$data = $stdObj->viewData();
+
+if (isset(($_POST['submit']))) {
+    $stdObj->add();
+}
+
+// echo var_dump($data);
+
+
+// class Fruit
+// {
+//     public $name;
+//     public $color;
+
+//     function __construct($name)
+//     {
+//         $this->name = $name;
+//     }
+//     function get_name()
+//     {
+//         return $this->name;
+//     }
+// }
+
+// $apple = new Fruit("Apple");
+// echo $apple->get_name();
+
+// $banana = new Fruit("Banana");
+
+// echo $banana->get_name();
 
 ?>
 
@@ -23,9 +55,80 @@ include("function.php");
             <h2>PHP CRUD Appplication </h2>
         </header>
 
+        <div class="section">
+            <h1>Add New Students </h1>
+            <form action="index.php" method="POST">
+                <div class="form-group">
+                    <label for="first_name">First Name</label>
+                    <input type="text" class="form-control" name="first_name" id="first_name" aria-describedby="first_name" placeholder="Enter First Name">
+
+                </div>
+                <div class="form-group">
+                    <label for="last_name">Last Name</label>
+                    <input type="text" class="form-control" name="last_name" id="last_name" aria-describedby="last_name" placeholder="Enter Last Name">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email address</label>
+                    <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="phone">Phone</label>
+                    <input type="text" class="form-control" name="phone" id="phone" aria-describedby="phone #" placeholder="Enter Phone">
+                </div>
+                <div class="form-group">
+                    <label for="address">Address</label>
+                    <input type="text" class="form-control" name="address" id="address" aria-describedby="Address " placeholder="Enter Address">
+                </div>
+                <div class="form-group">
+                    <label for="course">Course</label>
+                    <input type="text" class="form-control" name="course" id="course" aria-describedby="Course " placeholder="Enter Course">
+                </div>
+
+                <input type="submit" style="margin:20px;" name="submit" value="Add Record" class="btn btn-danger">
+
+
+            </form>
+        </div>
+
+
         <main>
 
-            <table>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Serial # </th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Course</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($data as $student) {
+
+                    ?>
+                        <tr>
+                            <th scope="row"><?php echo $student['id'] ?></th>
+                            <td><?php echo $student['first_name'] ?></td>
+                            <td><?php echo $student['last_name'] ?></td>
+                            <td><?php echo $student['phone'] ?></td>
+                            <td><?php echo $student['email'] ?></td>
+                            <td><?php echo $student['address'] ?></td>
+                            <td><?php echo $student['course'] ?></td>
+                            <td><a href="edit.php?edit_id=<?php echo $student['id']; ?>"><i class="fas fa-edit"> </i></a>
+                                <a href="index.php?del_id=?<?php echo $student['id']; ?>"><i class="fas fa-trash"></i></a>
+                            </td>
+
+
+
+                        </tr>
+                    <?php } ?>
+                </tbody>
 
             </table>
 
